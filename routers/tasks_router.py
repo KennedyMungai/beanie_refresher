9e8a32b9843c6ba00544284a3aa5ffe6693c41cf
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-11 11:18:18 +0300

    Wrote the skeleton of the get_all_tasks router

--- routers/tasks_router.py
+++ routers/tasks_router.py
@@ -3,3 +3,9 @@ from fastapi import APIRouter
 
 
 tasks_router = APIRouter(prefix="/tasks", tags=["tasks"])
+
+
+@tasks_router.get("/")
+async def get_all_tasks():
+    """Get all tasks"""
+    return {"tasks": []}
