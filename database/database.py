723cf63e682c108ed56271977fcb8d283b4a8284
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-11 12:02:49 +0300

    Updated the db connection

--- database/database.py
+++ database/database.py
@@ -1,12 +1,18 @@
 """Will contain the db connection logic"""
 import motor
+import os
 from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv, find_dotenv
 
 from models.tasks_model import Task
+
+load_dotenv(find_dotenv())
+
+MONGODB_URL = os.environ.get("MONGODB_URL")
 
 
 async def init_db():
     """Handles the database connection logic"""
-    db_client = AsyncIOMotorClient("mongodb://localhost:27017").tasks
+    db_client = AsyncIOMotorClient(MONGODB_URL).tasks
     await init_beanie(database=db_client, document_models=[Task])
