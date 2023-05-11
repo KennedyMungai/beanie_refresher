388098a3bccfc9622df875e5084a8b092a3ab185
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-11 11:21:50 +0300

    Deleted the docstrings for the varioyus functions

--- routers/tasks_router.py
+++ routers/tasks_router.py
@@ -7,19 +7,16 @@ tasks_router = APIRouter(prefix="/tasks", tags=["tasks"])
 
 @tasks_router.get("/tasks")
 async def get_all_tasks():
-    """Get all tasks"""
     return {"tasks": []}
 
 
 @tasks_router.get("/tasks/{task_id}")
 async def get_single_router(task_id: int):
-    """Get a single task"""
     return {"task": task_id}
 
 
 @tasks_router.post("/tasks")
 async def create_router():
-    """Create a task"""
     return {"task": "created"}
 
 
