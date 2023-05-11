53c96ac82e7eea75d4d7d6f01acebc92da4b38e4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-11 11:51:38 +0300

    Created a function to initialize database connection

--- database/database.py
+++ database/database.py
@@ -1,3 +1,10 @@
 """Will contain the db connection logic"""
 import motor
 from motor.motor_asyncio import AsyncIOMotorClient
+from beanie import init_beanie
+from models.tasks_model import Task
+
+
+async def init_db():
+    db_client = AsyncIOMotorClient("mongodb://localhost:27017")
+    init_beanie(database="Tasks", document_models=[Task])
