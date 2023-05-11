f17ea3e67b0b7c28624db72b65e3fd84df83918c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-11 11:23:08 +0300

    Refactored the router file

--- routers/tasks_router.py
+++ routers/tasks_router.py
@@ -5,26 +5,26 @@ from fastapi import APIRouter
 tasks_router = APIRouter(prefix="/tasks", tags=["tasks"])
 
 
-@tasks_router.get("/tasks")
-async def get_all_tasks():
+@tasks_router.get("/")
+async def get_all_tasks_router():
     return {"tasks": []}
 
 
-@tasks_router.get("/tasks/{task_id}")
-async def get_single_router(task_id: int):
+@tasks_router.get("/{task_id}")
+async def get_single_task_router(task_id: int):
     return {"task": task_id}
 
 
-@tasks_router.post("/tasks")
-async def create_router():
+@tasks_router.post("/")
+async def create_task_router():
     return {"task": "created"}
 
 
-@tasks_router.put("/tasks/{task_id}")
-async def update_router(task_id: int):
+@tasks_router.put("/{task_id}")
+async def update_task_router(task_id: int):
     return {"task": task_id}
 
 
-@tasks_router.delete("/tasks/{task_id}")
-async def delete_router(task_id: int):
+@tasks_router.delete("/{task_id}")
+async def delete_task_router(task_id: int):
     return {"task": task_id}
