75a6a5384db8276f799b87bf3d55c08681742b76
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-11 11:41:42 +0300

    Added a docstring for the Task model

--- models/tasks_model.py
+++ models/tasks_model.py
@@ -6,6 +6,11 @@ from pydantic import Field
 
 
 class Task(Document):
+    """The task model
+
+    Args:
+        Document (Beanie): The base document class
+    """
     task_content: str
     is_complete: bool
     date_created: datetime
