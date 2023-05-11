0d0ff71238e99fde0e83b57b8f17592fa3f77ad6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-11 11:05:23 +0300

    Created the root endpoint for the application backend

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
 """Created the entrypoint for the application"""
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/", name="Home", description="The root endpoint for the application", tags=["Home"])
+async def root():
+    """The root endpoint for the application"""
+    return {"message": "Hello World"}
