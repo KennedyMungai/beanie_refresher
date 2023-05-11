e48c587fdbe6e9284bf4cbfb1a0ff972d8fc1a7b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-11 11:56:33 +0300

    Called the init_beanie function asyncronously

--- database/database.py
+++ database/database.py
@@ -9,4 +9,4 @@ from models.tasks_model import Task
 async def init_db():
     """Handles the database connection logic"""
     db_client = AsyncIOMotorClient("mongodb://localhost:27017").tasks
-    init_beanie(database=db_client, document_models=[Task])
+    await init_beanie(database=db_client, document_models=[Task])
