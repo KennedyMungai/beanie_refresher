196df650860e3067e33f63c6087dc6f9d440daa2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-11 11:21:10 +0300

    Wrote the skeletons for all endpoints which perform all the CRUD operations

--- routers/tasks_router.py
+++ routers/tasks_router.py
@@ -5,7 +5,29 @@ from fastapi import APIRouter
 tasks_router = APIRouter(prefix="/tasks", tags=["tasks"])
 
 
-@tasks_router.get("/")
+@tasks_router.get("/tasks")
 async def get_all_tasks():
     """Get all tasks"""
     return {"tasks": []}
+
+
+@tasks_router.get("/tasks/{task_id}")
+async def get_single_router(task_id: int):
+    """Get a single task"""
+    return {"task": task_id}
+
+
+@tasks_router.post("/tasks")
+async def create_router():
+    """Create a task"""
+    return {"task": "created"}
+
+
+@tasks_router.put("/tasks/{task_id}")
+async def update_router(task_id: int):
+    return {"task": task_id}
+
+
+@tasks_router.delete("/tasks/{task_id}")
+async def delete_router(task_id: int):
+    return {"task": task_id}
