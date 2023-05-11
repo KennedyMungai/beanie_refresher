a7c92bf7d89f2720dae7988afd9903fe7fe2792d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-11 11:52:31 +0300

    Added a docstrong to the init_db function'

--- database/database.py
+++ database/database.py
@@ -7,5 +7,6 @@ from models.tasks_model import Task
 
 
 async def init_db():
+    """Handles the database connection logic"""
     db_client = AsyncIOMotorClient("mongodb://localhost:27017")
     init_beanie(database="Tasks", document_models=[Task])
